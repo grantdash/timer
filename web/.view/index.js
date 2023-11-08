f361adfda20996c778ad78fb36aac83f4522f673
zbryikt <tkirby@gmail.com>
2023-11-08 16:11:25 +0800

    init commit

---
+++ web/.view/index.js
@@ -0,0 +1,183 @@
+ (function() { function pug_attr(t,e,n,r){if(!1===e||null==e||!e&&("class"===t||"style"===t))return"";if(!0===e)return" "+(r?t:t+'="'+t+'"');var f=typeof e;return"object"!==f&&"function"!==f||"function"!=typeof e.toJSON||(e=e.toJSON()),"string"==typeof e||(e=JSON.stringify(e),n||-1===e.indexOf('"'))?(n&&(e=pug_escape(e))," "+t+'="'+e+'"'):" "+t+"='"+e.replace(/'/g,"&#39;")+"'"}
+function pug_escape(e){var a=""+e,t=pug_match_html.exec(a);if(!t)return e;var r,c,n,s="";for(r=t.index,c=0;r<a.length;r++){switch(a.charCodeAt(r)){case 34:n="&quot;";break;case 38:n="&amp;";break;case 60:n="&lt;";break;case 62:n="&gt;";break;default:continue}c!==r&&(s+=a.substring(c,r)),c=r+1,s+=n}return c!==r?s+a.substring(c,r):s}
+var pug_match_html=/["&<>]/;function template(locals) {var pug_html = "", pug_mixins = {}, pug_interp;;
+    var locals_for_with = (locals || {});
+    
+    (function (Array, c, defer, hashfile, libLoader, md5, url, version) {
+      pug_html = pug_html + "\u003C!DOCTYPE html\u003E";
+if(!libLoader) {
+  libLoader = {
+    js: {url: {}},
+    css: {url: {}},
+    root: function(r) { libLoader._r = r; },
+    _r: "/assets/lib",
+    _v: "",
+    version: function(v) { libLoader._v = (v ? "?v=" + v : ""); }
+  }
+  if(version) { libLoader.version(version); }
+}
+
+pug_mixins["script"] = pug_interp = function(os,cfg){
+var block = (this && this.block), attributes = (this && this.attributes) || {};
+var str = '', urls = [];
+if(!Array.isArray(os)) { os = [os]; }
+// iterate os
+;(function(){
+  var $$obj = os;
+  if ('number' == typeof $$obj.length) {
+      for (var pug_index0 = 0, $$l = $$obj.length; pug_index0 < $$l; pug_index0++) {
+        var o = $$obj[pug_index0];
+c = o;
+if(typeof(o) == "string") { url = o; c = cfg || {};}
+else if(o.url) { url = o.url; }
+else { url = libLoader._r + "/" + o.name + "/" + (o.version || 'main') + "/" + (o.path || "index.min.js"); }
+if (!libLoader.js.url[url]) {
+libLoader.js.url[url] = true;
+defer = (typeof(c.defer) == "undefined" ? true : !!c.defer);
+if (/^https?:\/\/./.exec(url)) {
+pug_html = pug_html + "\u003Cscript" + (" type=\"text\u002Fjavascript\""+pug_attr("src", url, true, true)+pug_attr("defer", defer, true, true)+pug_attr("async", !!c.async, true, true)) + "\u003E\u003C\u002Fscript\u003E";
+}
+else
+if (cfg && cfg.pack) {
+str = str + ';' + url;
+urls.push(url);
+}
+else {
+pug_html = pug_html + "\u003Cscript" + (" type=\"text\u002Fjavascript\""+pug_attr("src", url + libLoader._v, true, true)+pug_attr("defer", defer, true, true)+pug_attr("async", !!c.async, true, true)) + "\u003E\u003C\u002Fscript\u003E";
+}
+}
+      }
+  } else {
+    var $$l = 0;
+    for (var pug_index0 in $$obj) {
+      $$l++;
+      var o = $$obj[pug_index0];
+c = o;
+if(typeof(o) == "string") { url = o; c = cfg || {};}
+else if(o.url) { url = o.url; }
+else { url = libLoader._r + "/" + o.name + "/" + (o.version || 'main') + "/" + (o.path || "index.min.js"); }
+if (!libLoader.js.url[url]) {
+libLoader.js.url[url] = true;
+defer = (typeof(c.defer) == "undefined" ? true : !!c.defer);
+if (/^https?:\/\/./.exec(url)) {
+pug_html = pug_html + "\u003Cscript" + (" type=\"text\u002Fjavascript\""+pug_attr("src", url, true, true)+pug_attr("defer", defer, true, true)+pug_attr("async", !!c.async, true, true)) + "\u003E\u003C\u002Fscript\u003E";
+}
+else
+if (cfg && cfg.pack) {
+str = str + ';' + url;
+urls.push(url);
+}
+else {
+pug_html = pug_html + "\u003Cscript" + (" type=\"text\u002Fjavascript\""+pug_attr("src", url + libLoader._v, true, true)+pug_attr("defer", defer, true, true)+pug_attr("async", !!c.async, true, true)) + "\u003E\u003C\u002Fscript\u003E";
+}
+}
+    }
+  }
+}).call(this);
+
+if (cfg && cfg.pack) {
+var name = md5(str);
+//var filename = "/js/pack/" + name + "." + (typeof(cfg.min) == "undefined" || cfg.min ? "min" : "") + ".js";
+var fn = "/assets/bundle/" + name + "." + (typeof(cfg.min) == "undefined" || cfg.min ? "min" : "") + ".js";
+hashfile({type: "js", name: name, files: urls, src: locals.filename});
+pug_html = pug_html + "\u003Cscript" + (" type=\"text\u002Fjavascript\""+pug_attr("src", fn + libLoader._v, true, true)) + "\u003E\u003C\u002Fscript\u003E";
+}
+};
+pug_mixins["css"] = pug_interp = function(os,cfg){
+var block = (this && this.block), attributes = (this && this.attributes) || {};
+var str = '', urls = [];
+if(!Array.isArray(os)) { os = [os]; }
+// iterate os
+;(function(){
+  var $$obj = os;
+  if ('number' == typeof $$obj.length) {
+      for (var pug_index1 = 0, $$l = $$obj.length; pug_index1 < $$l; pug_index1++) {
+        var o = $$obj[pug_index1];
+c = o;
+if(typeof(o) == "string") { url = o; c = cfg || {};}
+else if(o.url) { url = o.url; }
+else { url = libLoader._r + "/" + o.name + "/" + (o.version || 'main') + "/" + (o.path || "index.min.css"); }
+if (!libLoader.css.url[url]) {
+libLoader.css.url[url] = true;
+if (/^https?:\/\/./.exec(url)) {
+pug_html = pug_html + "\u003Clink" + (" rel=\"stylesheet\" type=\"text\u002Fcss\""+pug_attr("href", url, true, true)) + "\u003E";
+}
+else
+if (cfg && cfg.pack) {
+str = str + ';' + url;
+urls.push(url);
+}
+else {
+pug_html = pug_html + "\u003Clink" + (" rel=\"stylesheet\" type=\"text\u002Fcss\""+pug_attr("href", url + libLoader._v, true, true)) + "\u003E";
+}
+}
+      }
+  } else {
+    var $$l = 0;
+    for (var pug_index1 in $$obj) {
+      $$l++;
+      var o = $$obj[pug_index1];
+c = o;
+if(typeof(o) == "string") { url = o; c = cfg || {};}
+else if(o.url) { url = o.url; }
+else { url = libLoader._r + "/" + o.name + "/" + (o.version || 'main') + "/" + (o.path || "index.min.css"); }
+if (!libLoader.css.url[url]) {
+libLoader.css.url[url] = true;
+if (/^https?:\/\/./.exec(url)) {
+pug_html = pug_html + "\u003Clink" + (" rel=\"stylesheet\" type=\"text\u002Fcss\""+pug_attr("href", url, true, true)) + "\u003E";
+}
+else
+if (cfg && cfg.pack) {
+str = str + ';' + url;
+urls.push(url);
+}
+else {
+pug_html = pug_html + "\u003Clink" + (" rel=\"stylesheet\" type=\"text\u002Fcss\""+pug_attr("href", url + libLoader._v, true, true)) + "\u003E";
+}
+}
+    }
+  }
+}).call(this);
+
+if (cfg && cfg.pack) {
+var name = md5(str);
+//var filename = "/css/pack/" + name + "." + (typeof(cfg.min) == "undefined" || cfg.min ? "min" : "") + ".css";
+var fn = "/assets/bundle/" + name + "." + (typeof(cfg.min) == "undefined" || cfg.min ? "min" : "") + ".css";
+hashfile({type: "css", name: name, files: urls, src: locals.filename});
+pug_html = pug_html + "\u003Clink" + (" rel=\"stylesheet\" type=\"text\u002Fcss\""+pug_attr("href", fn + libLoader._v, true, true)) + "\u003E";
+}
+};
+pug_html = pug_html + "\u003Chtml\u003E\u003Chead\u003E";
+pug_mixins["css"]([
+      {name: "bootstrap", path: "dist/css/bootstrap.min.css"},
+      {name: "@loadingio/bootstrap.ext"},
+      {name: "ldcover"}
+    ]);
+pug_html = pug_html + "\u003Cstyle type=\"text\u002Fcss\"\u003Ehtml,body{width:100%;height:100%;margin:0;padding:0}\u003C\u002Fstyle\u003E\u003Cstyle type=\"text\u002Fcss\"\u003E.timer{color:#fff;font-size:10em;font-family:monospace;font-weight:700}\u003C\u002Fstyle\u003E\u003C\u002Fhead\u003E\u003Cbody\u003E\u003Cdiv class=\"timer w-100 h-100 bg-dark d-flex align-items-center justify-content-center\"\u003E\u003Cspan ld=\"value\"\u003E\u003C\u002Fspan\u003E\u003C\u002Fdiv\u003E";
+pug_mixins["script"]([
+      {name: "bootstrap.native", path: "dist/bootstrap-native.min.js"},
+      {name: "proxise"},
+      {name: "@loadingio/debounce.js"},
+      {name: "@loadingio/ldquery"},
+      {name: "ldview"},
+      {name: "ldcover"}
+    ]);
+pug_html = pug_html + "\u003Cscript type=\"module\"\u003Evar view,wrap,value,runner,r;view=new ldview({root:document.body});wrap=function(t){var r,n,e;r=Math.floor(t\u002F3600);n=Math.floor(t%3600\u002F60);e=t%60;return[r,n,e].map(function(t){return(t+\"\").padStart(2,\"0\")}).join(\":\")};value=view.get(\"value\");runner=function(t){t==null&&(t={});this._sec=t.second;this._t={start:0,paused:0};this._running=false;return this};runner.prototype=import$(Object.create(Object.prototype),{play:function(){var r=this;this._t.start=(this._t.start||0)+Date.now()-(this._t.paused||0);this._t.paused=0;this._running=true;return requestAnimationFrame(function(t){return r.tick(t)})},pause:function(){this._running=false;return this._t.paused=Date.now()},reset:function(){this._t.start=this._t.paused=0;return this.play()},tick:function(t){var r,n=this;if(!this._running){return}requestAnimationFrame(function(t){return n.tick(t)});t=(r=this._sec-Math.floor((Date.now()-this._t.start)\u002F1e3))\u003E0?r:0;return value.innerText=wrap(t)}});r=new runner({second:120});r.play();function import$(t,r){var n={}.hasOwnProperty;for(var e in r)if(n.call(r,e))t[e]=r[e];return t}\u003C\u002Fscript\u003E\u003C\u002Fbody\u003E\u003C\u002Fhtml\u003E";
+    }.call(this, "Array" in locals_for_with ?
+        locals_for_with.Array :
+        typeof Array !== 'undefined' ? Array : undefined, "c" in locals_for_with ?
+        locals_for_with.c :
+        typeof c !== 'undefined' ? c : undefined, "defer" in locals_for_with ?
+        locals_for_with.defer :
+        typeof defer !== 'undefined' ? defer : undefined, "hashfile" in locals_for_with ?
+        locals_for_with.hashfile :
+        typeof hashfile !== 'undefined' ? hashfile : undefined, "libLoader" in locals_for_with ?
+        locals_for_with.libLoader :
+        typeof libLoader !== 'undefined' ? libLoader : undefined, "md5" in locals_for_with ?
+        locals_for_with.md5 :
+        typeof md5 !== 'undefined' ? md5 : undefined, "url" in locals_for_with ?
+        locals_for_with.url :
+        typeof url !== 'undefined' ? url : undefined, "version" in locals_for_with ?
+        locals_for_with.version :
+        typeof version !== 'undefined' ? version : undefined));
+    ;;return pug_html;}; module.exports = template; })() 
---
+++ web/src/pug/index.ls
@@ -0,0 +1,39 @@
+view = new ldview do
+  root: document.body
+
+
+wrap = (t) ->
+  h = Math.floor(t / 3600)
+  m = Math.floor((t % 3600) / 60)
+  s = t % 60
+  [h,m,s].map(-> "#it".padStart(2, "0")).join(':')
+
+
+value = view.get(\value)
+
+runner = (o={}) ->
+  @_sec = o.second
+  @_t = {start: 0, paused: 0}
+  @_running = false
+
+  @
+runner.prototype = Object.create(Object.prototype) <<< do
+  play: ->
+    @_t.start = (@_t.start or 0) + Date.now! - (@_t.paused or 0)
+    @_t.paused = 0
+    @_running = true
+    requestAnimationFrame((t) ~> @tick t)
+  pause: ->
+    @_running = false
+    @_t.paused = Date.now!
+  reset: ->
+    @_t.start = @_t.paused = 0
+    @play!
+  tick: (t) ->
+    if !@_running => return
+    requestAnimationFrame (t) ~> @tick t
+    t = (@_sec - Math.floor((Date.now! - @_t.start) / 1000)) >? 0
+    value.innerText = wrap t
+
+r = new runner second: 120
+r.play!
